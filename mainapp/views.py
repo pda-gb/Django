d527dea82aecd073e3c474c5de75843ceac4de69
Denis <pda.prostor@gmail.com>
2020-09-24 19:11:22 +0300

    1. Реализовать наследование шаблонов в проекте. .     +

--- mainapp/views.py
+++ mainapp/views.py
@@ -3,12 +3,21 @@ from django.shortcuts import render
 
 # Create your views here.
 def main(request):
-    return render(request, 'mainapp/index.html')
+    variable_date = {
+        'title': 'Магазин Подушек',
+    }
+    return render(request, 'mainapp/index.html', variable_date)
 
 
 def products(request):
-    return render(request, 'mainapp/product_list.html')
+     variable_date = {
+         'title': 'Товары',
+     }
+     return render(request, 'mainapp/product_list.html', variable_date)
 
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+     variable_date = {
+        'title': 'Контакты',
+     }
+     return render(request, 'mainapp/contact.html', variable_date)
