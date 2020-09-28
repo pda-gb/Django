c6cff88662fef6035175f25fe66b11b38d533050
Denis <pda.prostor@gmail.com>
2020-09-28 14:09:11 +0300

    add feature to admin

--- mainapp/models.py
+++ mainapp/models.py
@@ -5,6 +5,10 @@ class ProductCategory(models.Model):
     name = models.CharField(verbose_name='имя', max_length=64, unique=True)
     description = models.TextField(verbose_name='описание', blank=True)
 
+    class Meta:  # для отображения в админке имя таблицы
+        verbose_name = 'Категория товаров'
+        verbose_name_plural = 'Категории товаров'
+
     def __str__(self):
         return self.name
 
@@ -18,6 +22,10 @@ class Product(models.Model):
     description = models.TextField(verbose_name='описание продукта', blank=True)
     price = models.DecimalField(verbose_name='цена продукта', max_digits=8, decimal_places=2, default=0)
     quantity = models.PositiveIntegerField(verbose_name='количество на складе', default=0)
+
+    class Meta:  # для отображения в админке имя таблицы
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
 
     def __str__(self):
         return f"{self.name} ({self.category.name})"
