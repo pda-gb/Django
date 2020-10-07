a429021b5b415154b25e353ce5626ee464680a05
Denis <pda.prostor@gmail.com>
2020-10-07 22:15:43 +0300

    add products to database

--- mainapp/management/commands/make_fill_db.py
+++ mainapp/management/commands/make_fill_db.py
@@ -1,4 +1,5 @@
-# создание быстрых команд для выполнения частых задач ($: python manage.py make_fill_db.py )
+# создание быстрых команд для выполнения частых задач ($: python manage.py make_fill_db )
+# если из json файла в базу добавляется в поле с  UNIQUE=True уже имеющееся значение, то будет ошибка
 import json
 import os
 
@@ -22,14 +23,15 @@ def load_from_json(file_name):
 
 class Command(BaseCommand):  # свой класс унаследуем от BaseCommand
     def handle(self, *args, **options):  # с обязательным методом handle
-        categories = load_from_json("categories")
-        for itm in categories:
-            ProductCategory.objects.create(**itm)  # распаковка словаря соответственно модели и распред. по ключам с
-            # одновременным сохр. в базу( .save() )
 
-        pr_types = load_from_json("types")
-        for itm in pr_types:
-            ProductType.objects.create(**itm)
+        # categories = load_from_json("categories")
+        # for itm in categories:
+        #     ProductCategory.objects.create(**itm)  # распаковка словаря соответственно модели и распред. по ключам с
+        #     # одновременным сохр. в базу( .save() )
+        #
+        # pr_types = load_from_json("types")
+        # for itm in pr_types:
+        #     ProductType.objects.create(**itm)
 
         products = load_from_json("products")
         for itm in products:
@@ -39,4 +41,3 @@ class Command(BaseCommand):  # свой класс унаследуем от Bas
             itm["type"] = type_name
             itm["category"] = category_name
             Product.objects.create(**itm)
-
