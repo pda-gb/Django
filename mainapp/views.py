ef3726f5229f3b89afd4b320dbc1b7d780cccf3a
Denis <pda.prostor@gmail.com>
2020-10-08 15:40:48 +0300

    2. Реализовать механизм вывода товаров по категориям.    +

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render
-import mainapp.models
+from django.shortcuts import render, get_object_or_404
+from mainapp.models import Product, ProductCategory, ProductType
 
 
 # Create your views here.
 def main(request):
     title = 'Магазин Подушек'
 
-    obj_products = mainapp.models.Product.objects.all()[:3]  # выведем на главной 3  товара
+    obj_products = Product.objects.all()[5:8]  # выведем на главной 3  товара
 
     variable_date = {
         'title': title,
@@ -14,11 +14,30 @@ def main(request):
     }
     return render(request, 'mainapp/index.html', variable_date)
 
-
+# сделать фильтрацию по типам последовательно после категорий
+# добавить третье меню для выбора типа и направления сортировки (название, цена, скидки)
 def products(request, pr_key=None):
     title = 'Товары'
-    links_menu_type = mainapp.models.ProductType.objects.all()
-    links_menu_category = mainapp.models.ProductCategory.objects.all()
+    links_menu_type = ProductType.objects.all()
+    links_menu_category = ProductCategory.objects.all()
+
+    if pr_key is not None:
+        if pr_key == 0:
+            products_set = Product.objects.all()
+            category_of_products_set = {"name": "все!"}  # при выборе дрю категории или заходе на продукты, эта строка не загружается
+            # type_of_products_set = {"name": "все"}
+        else:
+            category_of_products_set = get_object_or_404(ProductCategory, pk=pr_key)
+            # type_of_products_set = get_object_or_404(ProductType, pk= #_key)
+            products_set = Product.objects.all().filter(category=category_of_products_set)
+        variable_date = {
+            'title': title,
+            'links_menu_category': links_menu_category,
+            'links_menu_type': links_menu_type,
+            "products_set": products_set,
+            "category_of_products_set": category_of_products_set
+        }
+        return render(request, 'mainapp/product_list.html', variable_date)
 
     variable_date = {
         'title': title,
