2815ab9d3bad7bc7807b08c9ffc435861a459276
Denis <pda.prostor@gmail.com>
2020-10-12 04:43:35 +0300

    3. Реализовать просмотр товара, скорректировать адреса в каталоге и на главной странице так, чтобы при нажатии на товар появлялась страница просмотра. Добавление товара в корзину теперь должно быть только с этой страницы.     +

--- basketapp/views.py
+++ basketapp/views.py
@@ -3,13 +3,16 @@ from basketapp.models import Basket
 from mainapp.models import Product
 
 
+def check_authorisations(_user):
+     # узнаём кто зашёл, если зареган, выводим количество товаров у "корзинки"
+    _basket_itm = None
+    if not _user.is_anonymous:
+        _basket_itm = Basket.objects.filter(buyer=_user)  # собираем товары из корзины, для отображ. кол.-ва
+    return _basket_itm
+
 def basket(request):
     title = 'корзина'
-    shop_user = request.user  # узнаём кто зашёл, если зареган, выводим количество товаров у "корзинки"
-    basket_itm = None
-    if not shop_user.is_anonymous:
-        basket_itm = Basket.objects.filter(buyer=request.user)  # собираем товары из корзины, для отображ. кол.-ва
-
+    basket_itm = check_authorisations(request.user)
     variable_date = {
         'title': title,
         'basket_itm': basket_itm
@@ -22,16 +25,16 @@ def basket_add(request, pk_add):
     product_itm = get_object_or_404(Product, pk=pk_add)
     basket_itm = Basket.objects.filter(buyer=request.user, product=product_itm).first()
     # если нет, то создаём в корзине
-    if not basket_itm:
-        basket_itm = Basket.objects.create(buyer=request.user, product=product_itm)
+    basket_itm = check_authorisations(request.user)
     # увелич. кол.-во
     basket_itm.quantity += 1
     basket_itm.save()
-    print('+++++3+++++++')
-    print(basket_itm)
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
 def basket_remove(request, pk_del):
     variable_date = {}
     return render(request, 'basketapp/basket.html', variable_date)
+
+    # print('+++++#+++++++')
+    # print()
--- mainapp/views.py
+++ mainapp/views.py
@@ -11,8 +11,9 @@ def get_trending_product():
     :return:
     """
     _products = Product.objects.all()
-    trend = random.sample(list(_products), 6) #[0]
+    trend = random.sample(list(_products), 6)  # [0]
     return trend
+
 
 def get_products_in_main_page():
     """
@@ -21,7 +22,7 @@ def get_products_in_main_page():
     """
     main_prod = random.sample(list(Product.objects.filter(category_id=1)), 3)
     return main_prod
-    
+
 
 # def get_same_products()
 #     """
@@ -38,13 +39,12 @@ def get_basket(shop_user):
         _basket_itm = Basket.objects.filter(buyer=shop_user)  # собираем товары из корзины, для отображ. кол.-ва
     return _basket_itm
 
+
 def main(request):
     title = 'Магазин Подушек'
     basket_itm = get_basket(request.user)  # узнаём кто зашёл, если зареган, выводим количество товаров у "корзинки"
     main_products = get_products_in_main_page()  # выведем на главной 3  товара
     trending_products = get_trending_product()  # выведем на главной 6 тренд. товара
-    print('+++++3+++++++')
-    print(trending_products)
     variable_date = {
         'title': title,
         'main_products': main_products,
@@ -53,21 +53,23 @@ def main(request):
     }
     return render(request, 'mainapp/index.html', variable_date)
 
+
 # сделать фильтрацию по типам последовательно после категорий
 # добавить третье меню для выбора типа и направления сортировки (название, цена, скидки)
-def products(request, pr_key=None):
+def products(request, pk_cat=None):
     title = 'Товары'
     links_menu_type = ProductType.objects.all()
     links_menu_category = ProductCategory.objects.all()
     products_set = Product.objects.all()
     basket_itm = get_basket(request.user)
 
-    if pr_key is not None:
-        if pr_key == 0:
-            category_of_products_set = {"name": "все!"}  # при выборе дрю категории или заходе на продукты, эта строка не загружается
+    if pk_cat is not None:
+        if pk_cat == 0:
+            category_of_products_set = {
+                "name": "все!"}  # при выборе дрю категории или заходе на продукты, эта строка не загружается
             # type_of_products_set = {"name": "все"}
         else:
-            category_of_products_set = get_object_or_404(ProductCategory, pk=pr_key)
+            category_of_products_set = get_object_or_404(ProductCategory, pk=pk_cat)
             # type_of_products_set = get_object_or_404(ProductType, pk= #_key)
             products_set = Product.objects.all().filter(category=category_of_products_set)
         variable_date = {
@@ -103,8 +105,13 @@ def contact(request):
 def single_product(request, pk_prod):
     single_prod = get_object_or_404(Product, pk=pk_prod)
     title = single_prod.name
+    basket_itm = get_basket(request.user)
     variable_date = {
         'title': title,
+        'basket_itm': basket_itm,
         'single_prod': single_prod
     }
-    return render(request, 'mainapp/.html', variable_date)
+    print('+++++#+++++++')
+    print(single_prod.id)
+    return render(request, 'mainapp/single_product.html', variable_date)
+
