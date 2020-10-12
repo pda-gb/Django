eeb90fb8d9763c16f8888d13a8b8c19d32821a41
Denis <pda.prostor@gmail.com>
2020-10-12 06:04:36 +0300

    2. Реализовать механизм просмотра содержимого корзины и удаления товаров из нее.     +

--- basketapp/views.py
+++ basketapp/views.py
@@ -3,16 +3,16 @@ from basketapp.models import Basket
 from mainapp.models import Product
 
 
-def check_authorisations(_user):
+def get_authorised_prod(_user):
      # узнаём кто зашёл, если зареган, выводим количество товаров у "корзинки"
     _basket_itm = None
     if not _user.is_anonymous:
-        _basket_itm = Basket.objects.filter(buyer=_user)  # собираем товары из корзины, для отображ. кол.-ва
+        _basket_itm = Basket.objects.filter(buyer=_user).order_by('product__category')  # собираем товары из корзины, для отображ. кол.-ва
     return _basket_itm
 
 def basket(request):
     title = 'корзина'
-    basket_itm = check_authorisations(request.user)
+    basket_itm = get_authorised_prod(request.user)
     variable_date = {
         'title': title,
         'basket_itm': basket_itm
@@ -25,7 +25,7 @@ def basket_add(request, pk_add):
     product_itm = get_object_or_404(Product, pk=pk_add)
     basket_itm = Basket.objects.filter(buyer=request.user, product=product_itm).first()
     # если нет, то создаём в корзине
-    basket_itm = check_authorisations(request.user)
+    basket_itm = get_authorised_prod(request.user)
     # увелич. кол.-во
     basket_itm.quantity += 1
     basket_itm.save()
@@ -33,8 +33,9 @@ def basket_add(request, pk_add):
 
 
 def basket_remove(request, pk_del):
-    variable_date = {}
-    return render(request, 'basketapp/basket.html', variable_date)
+    rm_product = get_object_or_404(Basket, pk=pk_del)
+    rm_product.delete()
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
     # print('+++++#+++++++')
     # print()
