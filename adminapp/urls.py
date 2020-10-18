15170890f9de3e78c5ab759043693ad9d8b6695e
Denis <pda.prostor@gmail.com>
2020-10-19 00:16:56 +0300

    1. Создать приложение админки и интегрировать его в проект    - prepare view.py

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('users/update/<int:pk>/', adminapp.user_update, name='user_update'),
     path('users/delete/<int:pk>/', adminapp.user_delete, name='user_delete'),
 
-    path('products/create/', adminapp.products_create, name='products_create'),
+    path('products/create/category/<int:pk>/', adminapp.products_create, name='products_create'),
     path('products/read/category/<int:pk>/', adminapp.products_read, name='products_read'),
     path('products/read/<int:pk>/', adminapp.product_read, name='product_read'),
     path('products/update/<int:pk>/', adminapp.products_update, name='products_update'),
--- adminapp/views.py
+++ adminapp/views.py
@@ -1,2 +1,100 @@
+from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render
 
+
+# users ===============
+from authapp.models import Buyer
+
+
+@user_passes_test(lambda x: x.is_superuser)
+def users_read(request):
+    title = 'adm/пользователи'
+    users_list = Buyer.objects.all()
+    content = {
+        'title': title,
+        'objects': users_list
+    }
+    return render(request, 'adminapp/admin.html', content)
+
+
+@user_passes_test(lambda x: x.is_superuser)
+def user_create(request):
+    pass
+
+
+@user_passes_test(lambda x: x.is_superuser)
+def user_update(request, pk):
+    pass
+
+
+@user_passes_test(lambda x: x.is_superuser)
+def user_delete(request, pk):
+    pass
+
+
+# products ===============
+@user_passes_test(lambda x: x.is_superuser)
+def products_read(request, pk):
+    pass
+
+
+@user_passes_test(lambda x: x.is_superuser)
+def product_read(request, pk):
+    pass
+
+
+@user_passes_test(lambda x: x.is_superuser)
+def products_create(request, pk):
+    pass
+
+
+@user_passes_test(lambda x: x.is_superuser)
+def products_update(request, pk):
+    pass
+
+
+@user_passes_test(lambda x: x.is_superuser)
+def products_delete(request, pk):
+    pass
+
+
+# prod_cat ===============
+@user_passes_test(lambda x: x.is_superuser)
+def prod_cat_read(request):
+    pass
+
+
+@user_passes_test(lambda x: x.is_superuser)
+def prod_cat_create(request):
+    pass
+
+
+@user_passes_test(lambda x: x.is_superuser)
+def prod_cat_update(request, pk):
+    pass
+
+
+@user_passes_test(lambda x: x.is_superuser)
+def prod_cat_delete(request, pk):
+    pass
+
+
+# prod_type ===============
+@user_passes_test(lambda x: x.is_superuser)
+def prod_type_read(request):
+    pass
+
+
+@user_passes_test(lambda x: x.is_superuser)
+def prod_type_create(request):
+    pass
+
+
+@user_passes_test(lambda x: x.is_superuser)
+def prod_type_update(request, pk):
+    pass
+
+
+@user_passes_test(lambda x: x.is_superuser)
+def prod_type_delete(request, pk):
+    pass
