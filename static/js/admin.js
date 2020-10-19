5087455379a089c498e6d5406b1d07c7143ab7ea
Denis <pda.prostor@gmail.com>
2020-10-19 06:59:43 +0300

    fix: перехода для редактирования объекта в админке

--- static/js/admin.js
+++ static/js/admin.js
@@ -1,6 +1,8 @@
-// скрипт перехода для редактирования объекта
+// скрипт перехода по ссылке объекта
+// кроме кнопки перехода для редактирования объекта link-edit
 window.onload = function () {
-    $('tr[data-href]').on("click", function () {
+    $("tr[data-href]:not(.link-edit)").on("click", function () {
         document.location = $(this).data('href');
+        console.log($(this).data('href'));
     });
 };
